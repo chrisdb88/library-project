5ccfe6fe19598638270d9d1bbad09b8387626d81
chrisdb88 <chrisdb88@proton.me>
2024-06-18 11:31:28 -0500

    Added lot of js

--- javascript.js
+++ javascript.js
@@ -1,38 +1,109 @@
 
+const childClone = document.querySelector('.gridChild').cloneNode(true);
+document.querySelector('.gridChild').remove()
+
 const bookGrid = document.querySelector(".bookGrid")
+const bookForm = document.querySelector("#bookForm")
+const bookModal = document.querySelector(".bookModal")
+const addButton = document.querySelector(".addButton")
+const cancelButton = document.querySelector('.cancelButton')
+const clearButton = document.querySelector('.clearButton')
+
+bookGrid.appendChild(childClone)
 
 const bookCollection = [];
 
 let newBookPos = 0;
 
-function constructBook(title, author, pages, read) {
-    this.title = title;
+function constructBook(author, title, pages, read) {
+    
     this.author = author;
+    this.title = title;
     this.pages = pages;
     this.read = read
-    
-    this.toggleRead = function() {
-        if (this.read === false) {
-            this.read = true
-        } else if (this.read === true) {
-            this.read = false
-        }
-    }
 
     this.pos = newBookPos
     this.identify = `book${this.pos}`
 
-    const newChild = document.createElement("div")
+    const newChild = childClone.cloneNode(true)
 
     newChild.className = "gridChild"
     newChild.id += this.identify
 
-    bookGrid.appendChild(newChild)
+    newChild.querySelector('.authorInfo').textContent = author;
+    newChild.querySelector('.titleInfo').textContent = title;
+    newChild.querySelector('.pagesInfo').textContent = pages;
 
+    if (this.read === false) {
+        newChild.querySelector('.readStatus').textContent = "No";
+        newChild.querySelector('.toggleReadButton').style.backgroundColor = "rgb(255, 68, 68)";
+    } else if (this.read === true) {
+        newChild.querySelector('.readStatus').textContent = "Yes";
+        newChild.querySelector('.toggleReadButton').style.backgroundColor = "rgb(150, 255, 150)";
+    }
+
+    newChild.querySelector('.toggleReadButton').addEventListener('click', () => {
+        if (this.read === false) {
+            this.read = true
+            newChild.querySelector('.readStatus').textContent = "Yes";
+            newChild.querySelector('.toggleReadButton').style.backgroundColor = "rgb(150, 255, 150)";
+        } else if (this.read === true) {
+            this.read = false
+            newChild.querySelector('.readStatus').textContent = "No";
+            newChild.querySelector('.toggleReadButton').style.backgroundColor = "rgb(255, 68, 68)";
+        }
+    })
+
+    bookGrid.appendChild(newChild)
+    
     newBookPos++
 
     this.delete = function() {
         const thisElement = document.getElementById(this.identify)
         thisElement.remove()
     }
+    bookCollection.push(this)
 }
+
+addButton.addEventListener("click", function() {
+    bookModal.showModal()
+})
+
+cancelButton.addEventListener("click", function() {
+    bookModal.close()
+})
+
+const authorInput = document.querySelector("#author")
+const titleInput = document.querySelector("#title")
+const pagesInput = document.querySelector("#pages")
+const readInput = document.querySelector("#read")
+
+bookForm.addEventListener("submit", function(e) {
+
+    e.preventDefault()
+    let pagesToNumber = Number(pagesInput.value)
+
+    if ((typeof authorInput.value === "string" && authorInput.value.length >= 1 && authorInput.value.length <= 20)
+        && (typeof titleInput.value === "string" && titleInput.value.length >= 1 && titleInput.value.length <= 20)
+        && (typeof pagesInput.value === "string" && pagesToNumber >= 1 && pagesToNumber <= 99999)
+        && (typeof readInput.checked === "boolean") && (readInput.checked === true || readInput.checked === false)) 
+        {
+            constructBook(authorInput.value, titleInput.value, pagesInput.value, readInput.checked)
+            bookModal.close()
+            bookForm.reset()
+        } else {
+            console.log("ERROR: Invalid input!")
+        }
+})
+
+clearButton.addEventListener('click', function() {
+    bookForm.reset()
+})
+
+
+
+
+
+
+
+
